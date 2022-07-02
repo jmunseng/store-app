ea2a5ff94ffc894f88f4e5316163b11a2de0c066
munseong jeong <jmunseng@gmail.com>
2022-07-03 07:16:12 +1000

    store app

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -18,3 +18,30 @@ export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
 
   return [value, setValue] as [typeof value, typeof setValue];
 }
+// import React, { useEffect, useState } from 'react';
+
+// // export default function useLocalStorage<T>(
+// //   key: string,
+// //   initialvalue: T | (() => T)
+// // ) {}
+
+// const useLocalStorage = <T>(key: string, initialvalue: T | (() => T)) => {
+//   const [value, setValue] = useState<T>(() => {
+//     const jsonValue = localStorage.getItem(key);
+//     if (jsonValue != null) {
+//       return JSON.parse(jsonValue);
+//     }
+//     if (typeof initialvalue === 'function') {
+//       return initialvalue as () => T;
+//     } else {
+//       initialvalue;
+//     }
+//   });
+
+// 	@@ -23,6 +17,4 @@ const useLocalStorage = <T>(key: string, initialvalue: T | (() => T)) => {
+//   }, [key, value]);
+
+//   return [value, setValue] as [typeof value, typeof setValue];
+// };
+
+// export default useLocalStorage;
