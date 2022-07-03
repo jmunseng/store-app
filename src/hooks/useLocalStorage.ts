1b5cd1f7e065fe823f902aa50ae6700198fdf65c
munseong jeong <jmunseng@gmail.com>
2022-07-03 14:50:40 +1000

    store app

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,47 +1,23 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
+// export default function useLocalStorage<T>(
+//   key: string,
+//   initialvalue: T | (() => T)
+// ) {}
+
+const useLocalStorage = <T>(key: string, initialvalue: T | (() => T)) => {
   const [value, setValue] = useState<T>(() => {
     const jsonValue = localStorage.getItem(key);
     if (jsonValue != null) return JSON.parse(jsonValue);
 
-    if (typeof initialValue === 'function') {
-      return (initialValue as () => T)();
+    if (typeof initialvalue === 'function') {
+      return (initialvalue as () => T)();
     } else {
-      return initialValue;
+      initialvalue;
     }
   });
 
-  useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
-  }, [key, value]);
-
   return [value, setValue] as [typeof value, typeof setValue];
-}
-// import React, { useEffect, useState } from 'react';
+};
 
-// // export default function useLocalStorage<T>(
-// //   key: string,
-// //   initialvalue: T | (() => T)
-// // ) {}
-
-// const useLocalStorage = <T>(key: string, initialvalue: T | (() => T)) => {
-//   const [value, setValue] = useState<T>(() => {
-//     const jsonValue = localStorage.getItem(key);
-//     if (jsonValue != null) {
-//       return JSON.parse(jsonValue);
-//     }
-//     if (typeof initialvalue === 'function') {
-//       return initialvalue as () => T;
-//     } else {
-//       initialvalue;
-//     }
-//   });
-
-// 	@@ -23,6 +17,4 @@ const useLocalStorage = <T>(key: string, initialvalue: T | (() => T)) => {
-//   }, [key, value]);
-
-//   return [value, setValue] as [typeof value, typeof setValue];
-// };
-
-// export default useLocalStorage;
+export default useLocalStorage;
